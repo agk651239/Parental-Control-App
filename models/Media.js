2281c07ac13d9009803d5e0759c932ed8afb4b43
A G K <agk651239@gmail.com>
2026-09-07 15:22:28 +0530

    Add Media model with schema definition

--- models/Media.js
+++ models/Media.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
+
+const mediaSchema = new mongoose.Schema({
+    deviceId: { type: String, required: true },
+    fileUrl: { type: String, required: true },
+    publicId: { type: String, required: true },
+    mediaType: { type: String, enum: ['image', 'video', 'audio', 'call_recording'], required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Media', mediaSchema);
 
